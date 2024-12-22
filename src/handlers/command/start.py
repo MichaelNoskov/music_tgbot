92938b711d661975629d923474aa93d548048299
MichaelNoskov <prosto.ya.chelovekn@mail.ru>
2024-12-22 14:20:25 +0300

    fix: исправляет зависимости

--- src/handlers/command/start.py
+++ src/handlers/command/start.py
@@ -1,4 +1,3 @@
-import msgpack
 import aio_pika
 from aiogram.filters import Command
 from aiogram.types import Message
@@ -12,29 +11,3 @@ from config.settings import settings
 @router.message(Command('start'))
 async def menu(message: Message) -> None:
     await message.answer(render('start.jinja2'))
-    async with channel_pool.acquire() as channel:
-        exchange = await channel.declare_exchange('user_music', aio_pika.ExchangeType.TOPIC, durable=True)
-        queue = await channel.declare_queue(
-            settings.USER_QUEUE.format(user_id=message.from_user.id),
-            durable=True
-        )
-        user_queue = await channel.declare_queue(
-            'user_messages',
-            durable=True
-        )
-
-        await queue.bind(exchange,
-                         settings.USER_QUEUE.format(user_id=message.from_user.id)
-                         )
-
-        await user_queue.bind(exchange, 'user_messages')
-
-        body = {
-            'user_id': message.from_user.id,
-            'action': 'login'
-        }
-
-        await exchange.publish(
-            aio_pika.Message(msgpack.packb(body)),
-            'user_messages'
-        )
