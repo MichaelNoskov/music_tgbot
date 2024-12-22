851e1fe356763108c93b015737db9776fa2f1312
MichaelNoskov <prosto.ya.chelovekn@mail.ru>
2024-12-22 13:46:00 +0300

    fix: забыли обновить settings

--- config/settings.py
+++ config/settings.py
@@ -3,6 +3,17 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
+    BOT_WEBHOOK_URL: str 
+ 
+    MINIO_PORT: int 
+    MINIO_API_PORT: int 
+    MINIO_ENDPOINT: str 
+    MINIO_ROOT_USER: str 
+    MINIO_ROOT_PASSWORD: str 
+    MINIO_BUCKET: str 
+    MINIO_LOCAL_STORAGE: str 
+    MINIO_ACCESS_KEY: str 
+    MINIO_SECRET_KEY: str 
 
     class Config:
         env_file = ".env"
