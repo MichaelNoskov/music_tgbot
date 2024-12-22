5ecb63f1ef16943bb14d39eb60efbd0554579f4f
MichaelNoskov <prosto.ya.chelovekn@mail.ru>
2024-12-22 15:00:15 +0300

    fix: убран функционал для будущей рекомендательной системы

--- src/model/music.py
+++ src/model/music.py
@@ -10,5 +10,5 @@ class Music(Base, UUIDMixin):
     title: Mapped[str] = mapped_column(nullable=False, index=True)
     author = relationship('User', back_populates='music', cascade='all, delete-orphan')
     genre: Mapped[str] = mapped_column(String)
-    vector: Mapped[int] = mapped_column(Integer)
+    # vector: Mapped[int] = mapped_column(Integer)
     file_url: Mapped[str] = mapped_column(nullable=False)  # URL к файлу в MinIO
