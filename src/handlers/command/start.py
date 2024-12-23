3bd69331d0776b91717fa195ac138a07d840de7d
KristevT <timkristev.cloud@gmail.com>
2024-12-23 16:23:17 +0300

    feat: rabbitmq in start.py

--- src/handlers/command/start.py
+++ src/handlers/command/start.py
@@ -1,7 +1,13 @@
 import aio_pika
+import msgpack
+
+from aio_pika import ExchangeType
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
+from src.schema.music import MusicMessage
+from src.handlers.states.auth import AuthGroup
 from src.handlers.command.router import router
 from src.templates.env import render
 from src.storage.rabbit import channel_pool
@@ -9,5 +15,44 @@ from config.settings import settings
 
 
 @router.message(Command('start'))
-async def menu(message: Message) -> None:
+async def start(message: Message, state: FSMContext) -> None:
+    async with channel_pool.acquire() as channel:   # type: aio_pika.Channel
+        exchange = await channel.declare_exchange("user_music", ExchangeType.TOPIC, durable=True)
+
+        queue = await channel.declare_queue(
+            settings.USER_QUEUE.format(
+                user_id=message.from_user.id,
+            ),
+            durable=True,
+        )
+
+        users_queue = await channel.declare_queue(
+            'user_messages',
+            durable=True,
+        )
+
+        await queue.bind(
+            exchange,
+            settings.USER_QUEUE.format(
+                user_id=message.from_user.id,
+            ),
+        )
+        await users_queue.bind(
+            exchange,
+            'user_messages'
+        )
+
+        await exchange.publish(
+            aio_pika.Message(
+                msgpack.packb(
+                    MusicMessage(
+                        user_id=message.from_user.id,
+                        action='get_music',
+                        event='gift'
+                    )
+                ),
+            ),
+            'user_messages'
+        )
+    
     await message.answer(render('start.jinja2'))
