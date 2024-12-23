d48ce52c0940319d0c794e470ae3b3db18a66a22
KristevT <timkristev.cloud@gmail.com>
2024-12-23 19:01:25 +0300

    fix: minio modified

--- src/storage/minio_.py
+++ src/storage/minio_.py
@@ -1,40 +1,49 @@
+import random
+
 from minio import Minio
 from minio.error import S3Error
 from config.settings import settings
-
-minio_client: Minio
-
-def setup_minio() -> Minio:
-    global minio_client
-
-    minio_client = Minio(
-        endpoint=settings.MINIO_ENDPOINT,
-        access_key=settings.MINIO_ACCESS_KEY,
-        secret_key=settings.MINIO_SECRET_KEY,
-        secure=False
-    )
-
-    return minio_client
-
-def get_minio() -> Minio:
-    global minio_client
-
-    if minio_client is None:
-        raise Exception("MinIO client is not initialized. Call setup_minio() first.")
-    
-    return minio_client
+from io import BytesIO
 
 
-async def upload_file(audio_file: str):
-    client = get_minio()
-    
+minio_client = Minio(
+    endpoint=settings.MINIO_ENDPOINT,
+    access_key=settings.MINIO_ACCESS_KEY,
+    secret_key=settings.MINIO_SECRET_KEY,
+    secure=False
+)
+
+async def upload_music(filename, audio_bytes):
     try:
-        if not client.bucket_exists(settings.MINIO_BUCKET):
-            client.make_bucket(settings.MINIO_BUCKET)
+        if not minio_client.bucket_exists(settings.MINIO_BUCKET):
+            minio_client.make_bucket(settings.MINIO_BUCKET)
 
-        client.fput_object(settings.MINIO_BUCKET, audio_file, settings.MINIO_LOCAL_STORAGE)
-        print(f"File {audio_file} uploaded to bucket {settings.MINIO_BUCKET}.")
+        minio_client.put_object(
+            bucket_name=settings.MINIO_BUCKET, 
+            object_name=filename, 
+            data=BytesIO(audio_bytes),
+            length=len(audio_bytes)
+        )
+        print(f"File {filename} uploaded to bucket {settings.MINIO_BUCKET}.")
     except S3Error as e:
         print(f"Error occurred: {e}")
+        return
 
-upload_file("A3 On The Run.flac")
+async def get_music(audiofile):
+    try:
+        minio_client.fget_object(settings.MINIO_BUCKET, audiofile, f"{settings.MINIO_LOCAL_STORAGE}{audiofile}")
+        print(f"File '{audiofile}' downloaded as 'f{settings.MINIO_LOCAL_STORAGE}{audiofile}'.")
+    except S3Error as e:
+        print(f"Error occurred: {e}")
+        return
+
+async def get_random_music():
+    objects = list(minio_client.list_objects(settings.MINIO_BUCKET))
+    
+    if not objects:
+        print("No available files.")
+        return None
+    
+    random_music = random.choice(objects)
+    
+    return random_music.object_name
